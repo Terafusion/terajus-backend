b789223796cae65fceeae63f46e8754ef9337959
Gabriel Oliveira <gabrielrbg8@outlook.com>
2024-03-01 11:08:01 -0300

    fix: document store request sintax error

--- app/Http/Requests/Document/DocumentStoreRequest.php
+++ app/Http/Requests/Document/DocumentStoreRequest.php
@@ -25,7 +25,7 @@ class DocumentStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'file' => 'required|mimes:pdf,doc,docx,csv,jpg,jpeg,png',
+            'file' => ['required', 'mimes:pdf,doc,docx,csv,jpg,jpeg,png'],
             'model_type' => ['required', 'string',  new ValidModelType],
             'model_id' => ['required', 'numeric'],
             'document_type_id' => ['nullable', 'numeric', 'exists:document_types,id'],
